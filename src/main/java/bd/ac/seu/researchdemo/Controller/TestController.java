55875181765e8630f055e19f6cbb83c8846f742f
rakib <rakibhasansabbir@yahoo.com>
2018-03-02 23:22:11 +0600

    update

--- src/main/java/bd/ac/seu/researchdemo/Controller/TestController.java
+++ src/main/java/bd/ac/seu/researchdemo/Controller/TestController.java
@@ -2,14 +2,22 @@ package bd.ac.seu.researchdemo.Controller;
 
 import bd.ac.seu.researchdemo.Models.TestUpload;
 import bd.ac.seu.researchdemo.repository.TestUploadDao;
+import org.aspectj.weaver.ast.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TestController {
@@ -25,9 +33,26 @@ public class TestController {
         return "test";
     }
 
-    @RequestMapping(value = "/test", method = RequestMethod.POST)
-    public String upload(@ModelAttribute @Valid TestUpload testUpload){
-        testUploadDao.save(new TestUpload(testUpload.getFile()));
+
+
+    @PostMapping(value = "/test")
+    public String upload(@RequestParam MultipartFile upl) {
+        try {
+            TestUpload testUpload = new TestUpload(upl.getBytes());
+            testUploadDao.save(testUpload);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "redirect:test";
     }
+
+    @RequestMapping(value = "/image/{image_id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage(@PathVariable("image_id") int imageId) throws IOException {
+
+        byte[] imageContent = testUploadDao.findOne(imageId).getFile();
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+        return new ResponseEntity<byte[]>(imageContent, headers, HttpStatus.OK);
+    }
+
 }
